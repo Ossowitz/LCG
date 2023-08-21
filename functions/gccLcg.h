455dde24c2457da8a886767d8f43076b2c6178ef
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-08-22 00:59:23 +0300

    Initial Commit

--- functions/gccLcg.h
+++ functions/gccLcg.h
@@ -3,7 +3,6 @@
 #define EXCEPTION                   "\033[1;31m%s\033[0m\n"
 #define STATELESS                   "\033[1;32m%s\033[0m\n"
 #define OPEN_FILE_EXCEPTION         "+ OPEN_FILE_EXCEPTION +"
-#define OUTPUT_FILE                 "output.txt"
 
 static long long m = 1 << 31;
 static long long a = 1103515245;
@@ -13,15 +12,22 @@ long long lcg(long long seed) {
     return (a * seed + c) % m;
 }
 
-void lcgWriteToFile(char *filename) {
+void lcgWriteToFile(char *filename, long long n) {
     FILE *output = fopen(filename, "w");
-    long long seed = 1 << 31;  // Начальное значение для ЛКГ
 
     if (output != NULL) {
-        fprintf(output,
-                "%lld ",
-                lcg(seed)
-        );
+        long long seed = 1 << 31;
+        long long sequence[n];
+
+        for (long long i = 0; i < n; i++) {
+            seed = lcg(seed);
+            sequence[i] = seed;
+            fprintf(output,
+                    "%lld ",
+                    sequence[i]
+            );
+        }
+
     } else {
         printf(EXCEPTION,
                OPEN_FILE_EXCEPTION
---
+++ main/Log.cpp
@@ -0,0 +1,5 @@
+#include "../functions/gccLcg.h"
+
+int main() {
+    lcgWriteToFile("test.txt", 10);
+}
