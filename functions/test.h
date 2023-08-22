8a748f4c12253ff26b41da92ba7d692c829808d0
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-08-22 12:18:08 +0300

    Implementation test() method

--- functions/test.h
+++ functions/test.h
@@ -5,7 +5,7 @@
 #define OPEN_FILE_EXCEPTION         "+ OPEN_FILE_EXCEPTION +"
 #define NO_SOLUTION_EXCEPTION       "no solution"
 
-#define SEQUENCE_LENGTH 100
+#define SEQUENCE_LENGTH 10000
 
 static long long m = 1 << 31;
 static long long a = 1103515245;
@@ -43,15 +43,53 @@ void lcgWriteToFile(char *filename) {
 
 void readFileToArray(FILE *file, long long int *array);
 
+double chiSquareTest(long long *observed, long long *expected, long long categories) {
+    double chiSquare = 0.0;
+
+    for (long long i = 0; i < categories; i++) {
+        chiSquare += pow(observed[i] - expected[i], 2) / expected[i];
+    }
+
+    return chiSquare;
+}
+
+void checkRandomness(long long sequence[], long long length) {
+    long long increasing = 0;
+    long long decreasing = 0;
+
+    for (long long i = 1; i < length + 1; i += 2) {
+        long long current = sequence[i];
+        long long previous = sequence[i - 1];
+        if (current > previous) {
+            increasing++;
+        } else if (current < previous) {
+            decreasing++;
+        }
+    }
+
+    printf("increasing: %lld\n", increasing);
+    printf("decreasing: %lld\n", decreasing);
+
+    long long observed[] = {increasing, decreasing};
+    long long expected[] = {
+            SEQUENCE_LENGTH / 4,
+            SEQUENCE_LENGTH / 4
+    };
+
+    double squareTest = chiSquareTest(observed, expected, 2);
+
+    printf("%f\n", squareTest);
+}
+
 void test(char *filename) {
     lcgWriteToFile(filename);
 
     FILE *file = fopen(filename, "r");
     if (file != NULL) {
+        long long sequence[SEQUENCE_LENGTH];
+        readFileToArray(file, sequence);
 
-        long long array[SEQUENCE_LENGTH];
-
-        readFileToArray(file, array);
+        checkRandomness(sequence, SEQUENCE_LENGTH);
 
     } else {
         printf(EXCEPTION,
