f900e3e2c056b5a1ccf079411a1b013684bc35f1
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-08-22 13:55:31 +0300

    Success get_с() method

--- functions/get_c.h
+++ functions/get_c.h
@@ -1,17 +1,35 @@
+/**
+ * @author: https://github.com/Ossowitz
+ * @author: https://t.me/DispatcherServlet
+ * TODO: implementation LKG
+ */
+
 #include <stdio.h>
 
 #define EXCEPTION                   "\033[1;31m%s\033[0m\n"
 #define OPEN_FILE_EXCEPTION         "+ OPEN_FILE_EXCEPTION +"
 #define OUTPUT_FILE                 "output.txt"
 
+/**
+ *
+ * @param   a - numeric
+ * @param   b - numeric
+ * @return: Finding the least common divisor
+ * @see:   https://www.youtube.com/watch?v=cn2geFx5xAI
+ */
 long long areCoprime(long long a, long long b) {
     if (b == 0) {
         return a == 1;
     }
-
     return areCoprime(b, a % b);
 }
 
+/**
+ * @param cmin - numeric
+ * @param cmax - numeric
+ * @param m    - numeric
+ * @note  Pick up all c relatively prime to m, 0 < c < m, cmin <= c <= cmax
+ */
 void get_c(long long cmin, long long cmax, long long m) {
     FILE *file = fopen(OUTPUT_FILE, "w");
     if (file != NULL) {
