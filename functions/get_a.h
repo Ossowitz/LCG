1ab0f694355765a2244e1fa4e5112f0e8609be2a
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-02 18:51:22 +0300

    Optimisation about prime numbers

--- functions/get_a.h
+++ functions/get_a.h
@@ -23,7 +23,7 @@
  */
 bool isPrime(long long n) {
     if (n > 1) {
-        for (long long i = 2; i < n; i++) {
+        for (long long i = 2; i <= sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
